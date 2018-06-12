0086e056818cda2aeb1bd17f7c7c1f544a1ee48e
Mostefaoui <m.mostefaoui@hotmail.fr>
2018-06-12 10:58:16 +0100

    Add project files.

---
+++ Tests/ReverseUtilTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tests
+{
+    using Utils;
+    using Xunit;
+
+    public class ReverseUtilTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var input = "Dotnet-bot: Welcome to using .NET Core!";
+            var expected = "!eroC TEN. gnisu ot emocleW :tob-tentoD";
+            var actual = ReverseUtil.ReverseString(input);
+            Assert.True(actual == expected, "The input string was not reversed correctly.");
+        }
+    }
+}
---
+++ Utils/ReverseUtil.cs
@@ -0,0 +1,15 @@
+﻿namespace Utils
+{
+    using System;
+
+    public static class ReverseUtil
+    {
+        public static string ReverseString(string input)
+        {
+            var chars = input.ToCharArray();
+            Array.Reverse(chars);
+            var reversedString = new string(chars);
+            return reversedString;
+        }
+    }
+}
